8ebce49a4af6e0589e52f14e8880c271de80dc49
Wallaceness <ncw232@gmail.com>
2020-01-24 09:38:02 -0500

    Design Work.

--- app/src/main/java/com/example/android/businesscard/FormActivity.java
+++ app/src/main/java/com/example/android/businesscard/FormActivity.java
@@ -1,6 +1,14 @@
 package com.example.android.businesscard;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
