bc147bf2d566871354c11f281559379cf1050704
Wallaceness <ncw232@gmail.com>
2020-01-24 11:50:39 -0500

    App passing data locally with intents.

--- app/src/main/java/com/example/android/businesscard/FormActivity.java
+++ app/src/main/java/com/example/android/businesscard/FormActivity.java
@@ -1,14 +1,54 @@
 package com.example.android.businesscard;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FormActivity extends AppCompatActivity {
 
+    private EditText nameField;
+    private EditText professionField;
+    private EditText emailField;
+    private EditText phoneField;
+
+    public static final String REPLY_NAME = "com.example.android.businesscard.extra.REPLY_NAME";
+    public static final String REPLY_PROFESSION = "com.example.android.businesscard.extra.REPLY_PROFESSION";
+    public static final String REPLY_EMAIL = "com.example.android.businesscard.extra.REPLY_EMAIL";
+    public static final String REPLY_PHONE = "com.example.android.businesscard.extra.REPLY_PHONE";
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.card_update_form);
+        Intent intent = getIntent();
+        nameField = findViewById(R.id.name_field);
+        nameField.setText(intent.getStringExtra(MainActivity.EXTRA_NAME));
+        professionField = findViewById(R.id.profession_field);
+        professionField.setText(intent.getStringExtra(MainActivity.EXTRA_PROFESSION));
+        emailField= findViewById(R.id.email_field);
+        emailField.setText(intent.getStringExtra(MainActivity.EXTRA_EMAIL));
+        phoneField = findViewById(R.id.phone_field);
+        phoneField.setText(intent.getStringExtra(MainActivity.EXTRA_PHONE));
+    }
+
+
+    public void saveForm(View view) {
+        Intent intent= new Intent();
+        String name= nameField.getText().toString();
+        String profession = professionField.getText().toString();
+        String email = emailField.getText().toString();
+        String phone = phoneField.getText().toString();
+
+        intent.putExtra(REPLY_NAME, name);
+        intent.putExtra(REPLY_PROFESSION, profession);
+        intent.putExtra(REPLY_EMAIL, email);
+        intent.putExtra(REPLY_PHONE, phone);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/android/businesscard/MainActivity.java
+++ app/src/main/java/com/example/android/businesscard/MainActivity.java
@@ -5,15 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int TEXT_REQUEST = 1;
+    public static final String EXTRA_NAME = "com.example.android.businesscard.extra.NAME";
+    public static final String EXTRA_PROFESSION = "com.example.android.businesscard.extra.PROFESSION";
+    public static final String EXTRA_EMAIL = "com.example.android.businesscard.extra.EMAIL";
+    public static final String EXTRA_PHONE = "com.example.android.businesscard.extra.PHONE";
 
     private TextView nameText;
     private TextView professionText;
-    private TextView contactText;
     private TextView emailText;
     private TextView phoneText;
 
@@ -23,18 +27,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         nameText= findViewById(R.id.name_text);
         professionText = findViewById(R.id.profession_text);
-        contactText = findViewById(R.id.contact_text);
         emailText = findViewById(R.id.email_text);
         phoneText= findViewById(R.id.phone_text);
     }
 
-    public void onEdit(){
+    public void onEdit(View view){
         Intent intent=new Intent(this, FormActivity.class);
+        String name= nameText.getText().toString();
+        String profession= professionText.getText().toString();
+        String email = emailText.getText().toString();
+        String phone = phoneText.getText().toString();
+
+        intent.putExtra(EXTRA_NAME, name);
+        intent.putExtra(EXTRA_PROFESSION, profession);
+        intent.putExtra(EXTRA_EMAIL, email);
+        intent.putExtra(EXTRA_PHONE, phone);
         startActivityForResult(intent, TEXT_REQUEST);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+            String name= data.getStringExtra(FormActivity.REPLY_NAME);
+            String profession= data.getStringExtra(FormActivity.REPLY_PROFESSION);
+            String email= data.getStringExtra(FormActivity.REPLY_EMAIL);
+            String phone= data.getStringExtra(FormActivity.REPLY_PHONE);
+
+            nameText.setText(name);
+            professionText.setText(profession);
+            emailText.setText(email);
+            phoneText.setText(phone);
+            }
+        }
     }
 }
